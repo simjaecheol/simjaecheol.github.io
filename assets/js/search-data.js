a646bbf338f32cd5009115ad2ed941c78a1713c3
simjaecheol <simjaecheol@naver.com>
2026-05-22 15:16:53 +0000

    Deploying to gh-pages from @ simjaecheol/simjaecheol.github.io@fae2ecc33bb04ec4067fe76babb01e27c8a83887 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -93,6 +93,11 @@ ninja.data = [{
           description: "Autonomous driving features development in Euro Truck Simulator 2.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/chosun_truck/";
+            },},{id: "projects-sdui-labs",
+          title: 'SDUI Labs',
+          description: "Generative Server-Driven UI (SDUI) Playground for Samsung.com e-commerce scenarios",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/sdui_labs/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
